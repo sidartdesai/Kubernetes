3b46cbf4b47afaef587ade647512461d09b03365
Sidharth Desai <sidartdesai@gmail.com>
2024-09-07 18:03:08 +0530

    Update K8s-Cluster-Upgrade.sh

--- K8s-Cluster-Administration/K8s-Cluster-Upgrade.sh
+++ K8s-Cluster-Administration/K8s-Cluster-Upgrade.sh
@@ -2,9 +2,30 @@
 kubeadm version
 #kubeadm version: &version.Info {Major:"1", Minor:"29", GitVersion:"v1.29.0"}
 
-#unhold the package kubeadm
-sudo apt-mark unhold kubeadm
-sudo apt-get update && sudo apt install -y kubeadm=1.29.8-1.1 && sudo apt-mark hold kubeadm
+#updagre the control plane
+#determine the version to be upgraded to
+sudo apt update && apt-cache madison kubeadm | tac
+   kubeadm | 1.29.0-1.1 | https://pkgs.k8s.io/core:/stable:/v1.29/deb  Packages
+   kubeadm | 1.29.1-1.1 | https://pkgs.k8s.io/core:/stable:/v1.29/deb  Packages
+   kubeadm | 1.29.2-1.1 | https://pkgs.k8s.io/core:/stable:/v1.29/deb  Packages
+   kubeadm | 1.29.3-1.1 | https://pkgs.k8s.io/core:/stable:/v1.29/deb  Packages
+   kubeadm | 1.29.4-2.1 | https://pkgs.k8s.io/core:/stable:/v1.29/deb  Packages
+   kubeadm | 1.29.5-1.1 | https://pkgs.k8s.io/core:/stable:/v1.29/deb  Packages
+   kubeadm | 1.29.6-1.1 | https://pkgs.k8s.io/core:/stable:/v1.29/deb  Packages
+   kubeadm | 1.29.7-1.1 | https://pkgs.k8s.io/core:/stable:/v1.29/deb  Packages
+   kubeadm | 1.29.8-1.1 | https://pkgs.k8s.io/core:/stable:/v1.29/deb  Packages
+
+#Upgrade kubeadm
+apt-mark unhold kubeadm && sudo apt-get update && apt-get install -y kubeadm='1.29.8-1.1' apt-mark hold kubeadm
+
+#Verify that the download works and has the expected version:
+kubeadm version
+
+#Verify the upgrade plan:
+kubeadm upgrade plan
+
+#Apply the upgrade
+sudo kubeadm upgrade apply v1.29.8
 
 #drain the nodes
 kubectl drain node --ignore-daemonsets
